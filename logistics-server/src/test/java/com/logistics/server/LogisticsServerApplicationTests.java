23e8de185ac665985ac307040db74c8a4b2b51ed
hun5w <1076813765@qq.com>
2026-02-01 21:03:42 +0800

    chore:优化全局.gitignore规则,清理IDE冗余文件

---
+++ logistics-server/src/test/java/com/logistics/server/LogisticsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
